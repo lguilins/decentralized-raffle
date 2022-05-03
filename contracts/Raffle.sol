730883e1d2166ea80b7f98cd6156a4e665a513d9
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2022-05-03 14:13:53 -0300

    implement checkUpkeep function
    this function determines if it is time to trigger a new raffle winner

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -13,12 +13,16 @@ contract Raffle {
 
     RaffleState public s_raffleState;
     uint256 public immutable i_entranceFee;
+    uint256 public immutable i_interval;
+    uint256 public s_lastTimestamp;
     address payable[] public s_players;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -32,5 +36,22 @@ contract Raffle {
 
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
+    }
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) 
+        public 
+        view 
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool isOpen = RaffleState.Open == s_raffleState;
+        bool timePassed = ((block.timestamp - s_lastTimestamp) > i_interval);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && isOpen && hasBalance);
+        return (upkeepNeeded, "0x0");
     }
 }

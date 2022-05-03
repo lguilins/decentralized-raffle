cdddcfb5dda7d6153a2e2876047767089ed8aeaa
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2022-05-03 15:07:24 -0300

    add @chainlink dependency + start implementing performUpkeep method

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,8 +2,11 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
+error Raffle__UpkeepNotNeeded();
 
 contract Raffle {
     enum RaffleState {
@@ -16,13 +19,32 @@ contract Raffle {
     uint256 public immutable i_interval;
     uint256 public s_lastTimestamp;
     address payable[] public s_players;
+    VRFCoordinatorV2Interface public immutable i_vrfCoordinator;
+    bytes32 public i_gasLane;
+    uint64 public i_subscriptionId;
+    uint32 public i_callbackGasLimit;
+
+    uint16 public constant REQUEST_CONFIRMATIONS = 3;
+    uint32 public constant NUM_WORDS = 1;
+
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinatorV2,
+        bytes32 gasLane, //keyhash - how much gas can we spend to call this random number
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimestamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -54,5 +76,22 @@ contract Raffle {
         bool hasPlayers = s_players.length > 0;
         upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
         return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if(!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded();
+        }
+        s_raffleState = RaffleState.Calculating;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 }
